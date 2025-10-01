e850ebd60717578ae019ca1d240683fa2b862216
MOF <101332404+MoiconK@users.noreply.github.com>
2025-10-01 11:35:27 +0200

    Update contarPalabras.java

--- contadorPalabras/src/main/java/contarPalabras/contarPalabras.java
+++ contadorPalabras/src/main/java/contarPalabras/contarPalabras.java
@@ -4,9 +4,7 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-/**
- *
- */
+
 public class contarPalabras {
     static void main(String[] args) {
 
